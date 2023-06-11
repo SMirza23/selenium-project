4ed9d2ae3f72942829cf36c5fb3212bcc0100dbe
adamar <aysun114@hotmail.com>
2023-06-11 11:56:17 -0500

    Navigations_GetMethods class added

--- src/test/java/com/cydeo/tests/review/week2/Navigations_GetMethods.java
+++ src/test/java/com/cydeo/tests/review/week2/Navigations_GetMethods.java
@@ -32,6 +32,8 @@ public class Navigations_GetMethods {
         WebDriver driver = new ChromeDriver();
         //WebDriver driver = new FirefoxDriver();
 
+        driver.manage().window().maximize();
+
         // 2. Go to: https://www.etsy.com
         driver.get("https://www.etsy.com");
 
@@ -47,14 +49,45 @@ public class Navigations_GetMethods {
 
         // 5. Write “selenium” in search box
         WebElement searchBox = driver.findElement(By.id("twotabsearchtextbox"));
+        searchBox.sendKeys("selenium");
 
         // if test steps was saying just search for "selenium you can use Keys.ENTER
-        searchBox.sendKeys("selenium"+ Keys.ENTER);
+       // searchBox.sendKeys("selenium"+ Keys.ENTER);
 
         // 6. Press search button to search
+       WebElement searchButton = driver.findElement(By.xpath("//input[@id='nav-search-submit-button']"));
+       searchButton.click();
 
         // 7. Verify title is "Amazon.com : selenium"
+        String actualTitle = driver.getTitle();
+        String expectedTitle = "Amazon.com : selenium";
+
+        if(actualTitle.equals(expectedTitle)){
+            System.out.println("Amazon search title verification passed!");
+        }else{
+            System.out.println("Amazon search title verification failed!");
+        }
+
         // 8. Verify url contains "selenium"
+        String actualURL = driver.getCurrentUrl();
+        String expectedURL = "selenium";
+
+        if(actualURL.contains(expectedURL)){
+            System.out.println("Amazon search URL verification passed!");
+        }else{
+            System.out.println("Amazon search URL verification failed!");
+        }
+
+        // if you have more than 1 tab/windows, it will only close currently open tab
+        // you can still use driver instance in other tabs/windows
+      driver.close();
+
+        // if you have multiple tab/windows open, it will close everything
+        // it will kill driver session and you will not be able to use driver anymore
+      driver.quit();
+
+
+
 
     }
 }
